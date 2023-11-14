188f0a204e95874e93ad5ca9dc177577c77e01a4
Frogerall <saliticoadrian@gmail.com>
2023-11-14 09:04:35 +0800

    added turn string to uppercase

---
+++ script/uppercase.js
@@ -0,0 +1,42 @@
+// Change every other letter to Upper Case
+// let inputInfo = document.getElementById("para");
+// let cartItem = 0;
+// let resultBtn = document.querySelector(".result-btn");
+
+// resultBtn.addEventListener("click", () => {
+//  const dynamicValue = inputInfo.value;
+//  document.querySelector(".result").textContent += " " + dynamicValue;
+//  cartItem++;
+//  document.querySelector(".cart").textContent += " " + cartItem;
+// });
+
+// let age = (string) => {
+//  let newAge = "";
+//  for (let i = 0; i < string.length; i++) {
+//   if (i % 2 === 0) {
+//    newAge += string[i].toUpperCase();
+//   } else {
+//    newAge += string[i].toLowerCase();
+//   }
+//  }
+//  return newAge;
+// };
+let buttonGet = document.querySelector(".result-btn");
+
+buttonGet.addEventListener("click", () => {
+ let inputValue = document.getElementById("para").value;
+ let newValue = "";
+ let cartItem = null;
+
+ for (let i = 0; i < inputValue.length; i++) {
+  if (i % 2 === 0) {
+   newValue += inputValue[i].toUpperCase();
+  } else {
+   newValue += inputValue[i].toLowerCase();
+  }
+  cartItem++;
+ }
+
+ document.querySelector(".result").textContent += " " + newValue;
+});
+console.log(cartItem);
